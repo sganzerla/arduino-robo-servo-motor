6a9050bb683e6db8a393533e36abcc75521c84cf
alisson sganzerla <alisson_sganzerla@yahoo.com.br>
2020-03-31 00:49:48 -0300

    refatorando codigo

--- arduino-robo-servo-motor.ino
+++ arduino-robo-servo-motor.ino
@@ -1,50 +1,102 @@
 #include <VarSpeedServo.h>
+#include <stdlib.h>
 
 // create servo objects
 VarSpeedServo servoOlhos;
+int ANG_MAX_MIN_OLHOS[] = {30, 50};
+
 VarSpeedServo servoBoca;
+int ANG_MAX_MIN_BOCA[] = {50, 90};
+
 // velocidade dos movimentos
-int SPEED = 50;
+#define VEL_MOVIMENTO 50
+
+#define SEN_SOM_PIN 11
+
+// Essas variáveis definem alguns parâmetros do programa e auxiliam na detecção e contagem das palmas.
+int delayfinal = 100;      // Valor representa um tempo em milissegundos, esse tempo é aguardado pelo programa para que se inicie novamente o loop.
+int duracaoPalma = 200;    // Valor representa um tempo em milissegundos, é o tempo que dura o som de uma palma, precisa ser calibrado entre 100 e 250.
+int intervaloPalmas = 500; // Valor representa um tempo em milissegundos, é o intervalo máximo permitido entre uma sequência de palmas.
+int quantidadePalmas = 0;  // Quantidade de palmas registradas.
+long momentoPalma = 0;     // Marcador usado para a detecção das palmas, será utilizado junto com a função millis.
+long esperaPalmas = 0;     // Marcador usado para contagem dos intervalos de tempo, será utilizado junto com a função millis.
+
+bool emMovimentacao = false;
 
 void setup()
 {
-    // setando pino para servo e definindo grau min e máximo (pino, angulo minimo, angulo maximo)
-    servoOlhos.attach(13, 40, 100);
-    servoBoca.attach(12, 40, 100);
+    setandoPinos();
+    Serial.begin(9600);
 }
 
 void loop()
 {
+    if (!emMovimentacao)
+        escutarPalmas();
+}
 
-    movimentarOlhosBoca();
+void setandoPinos()
+{
+    // setando pino para servo e definindo grau min e máximo (pino, angulo minimo, angulo maximo)
+    servoOlhos.attach(7, ANG_MAX_MIN_OLHOS[0], ANG_MAX_MIN_OLHOS[1]);
+    servoBoca.attach(5, ANG_MAX_MIN_BOCA[0], ANG_MAX_MIN_BOCA[1]);
+
+    pinMode(SEN_SOM_PIN, INPUT);
+}
+
+void escutarPalmas()
+{
+    // Faz a leitura digital do sensor de som, quando este sensor detecta som ele desliga a porta de entrada, mudando seu estado para LOW e quando não detecta mantem em HIGH.
+    int leituraSom = digitalRead(SEN_SOM_PIN);
+
+    // Ações quando o sensor detectar som, ou seja, entrar em LOW.
+    if (leituraSom == HIGH)
+    {
+        // Marca o momento da palma, soma a quantidade de palmas e aguarda um intervalo para não marcar a mesma palma mais de uma vez.
+        if (momentoPalma == 0)
+        {
+            momentoPalma = esperaPalmas = millis();
+            quantidadePalmas = quantidadePalmas + 1;
+        }
+        else if ((millis() - momentoPalma) >= duracaoPalma)
+        {
+            momentoPalma = 0;
+        }
+    }
+
+    // Verifica se nenhuma palma mais pode ser dada
+    if (((millis() - esperaPalmas) > intervaloPalmas) && (quantidadePalmas != 0))
+    {
+        Serial.println(quantidadePalmas);
+        if (quantidadePalmas == 2)
+        {
+
+            movimentarOlhosBoca();
+        }
+
+        delay(delayfinal);    // Tempo de espera para continuar o programa, esse tempo é importante para evitar efeitos de possiveis detecções truncadas de ecos e reverberações no som.
+        quantidadePalmas = 0; // Retoma a condição inicial da quantidade de palmas.
+    }
 }
 
 void movimentarOlhosBoca()
 {
-    delay(1000);
+    // define como verdadeiro para impedir que o ruído do motor do servo
+    // invoque o método novamente
 
-    // (angulo, velocidade, aguarda movimento completo para ir proxima instrucao)
-    servoOlhos.write(40, SPEED, true);
-    servoBoca.write(40, SPEED, true);
+    emMovimentacao = true;
+ 
+    for (size_t i = 0; i < 5; i++) {
+        /*  (angulo, velocidade, aguarda movimento completo para ir proxima instrucao) */
+        servoOlhos.write(gerarNumeroRandomico(ANG_MAX_MIN_OLHOS), VEL_MOVIMENTO, true);
+        servoBoca.write(gerarNumeroRandomico(ANG_MAX_MIN_BOCA), VEL_MOVIMENTO, true);
+    };
 
-    servoOlhos.write(80, SPEED, true);
-    servoBoca.write(80, SPEED, true);
+    delay(2000);
+    emMovimentacao = false;
+}
 
-    servoOlhos.write(100, SPEED, true);
-    servoBoca.write(100, SPEED, true);
-
-    servoOlhos.write(60, SPEED, true);
-    servoBoca.write(60, SPEED, true);
-
-    servoOlhos.write(100, SPEED, true);
-    servoBoca.write(100, SPEED, true);
-
-    servoOlhos.write(70, SPEED, true);
-    servoBoca.write(70, SPEED, true);
-
-    servoOlhos.write(90, SPEED, true);
-    servoBoca.write(90, SPEED, true);
-
-    servoOlhos.write(50, SPEED, true);
-    servoBoca.write(50, SPEED, true);
+int gerarNumeroRandomico(int vetorMinMax[])
+{
+    return rand() % vetorMinMax[1] + vetorMinMax[0];
 }
